9ce50b6655bf05287f9ff47b86b3df10e6b8e212
Balaji-3009 <mbalaji3009@gmail.com>
2025-02-08 11:30:14 +0530

    updated send signal route

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ def get_signal():
 @app.route('/send_signal', methods=['POST'])
 def send_signal():
     global signal_state
-    data = request.json
-    signal_state = data.get("signal", 0)
+    signal_state = 1
     return jsonify({"message": "Signal updated", "current_signal": signal_state})
 
 @app.route('/reset_signal', methods=['POST'])
